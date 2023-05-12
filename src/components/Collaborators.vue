0e8eca2df035c13ce0d71682ddff706db4d86fa4
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2023-05-12 22:26:29 +0530

    fix: collab circle

--- src/components/Collaborators.vue
+++ src/components/Collaborators.vue
@@ -10,26 +10,25 @@ const { collaborators } = storeToRefs(store);
 const { user } = storeToRefs(useUserStore());
 </script>
 <template>
-  <div
-    class="cursor-pointer border p-5 rounded-xl hover:bg-gray-50 transition"
-    @click="user && openCollabModal()"
-  >
-    <h2 class="thin-text">Collaborators</h2>
-
-    <!-- Image Line -->
     <div
-      v-if="user && collaborators"
-      class="mt-3 flex items-center -space-x-2 image-line"
+        class="cursor-pointer border p-5 rounded-xl hover:bg-gray-50 transition"
+        @click="user && openCollabModal()"
     >
-      <img :key="user.id" :src="user.avatar_url" v-tooltip="user.login" />
-      <img
-        v-for="collaborator in collaborators"
-        :key="collaborator.id"
-        :src="collaborator.avatar_url"
-        v-tooltip="collaborator.login"
-      />
+        <h2 class="thin-text">Collaborators</h2>
+
+        <!-- Image Line -->
+        <div
+            v-if="user && collaborators"
+            class="mt-3 flex items-center -space-x-2 image-line"
+        >
+            <img
+                v-for="collaborator in collaborators"
+                :key="collaborator.id"
+                :src="collaborator.avatar_url"
+                v-tooltip="collaborator.login"
+            />
+        </div>
+        <p class="text-sm" v-else-if="!user">Log in to manage collaborators</p>
+        <div v-else class="mt-1 text-sm">No collaborators found</div>
     </div>
-    <p class="text-sm" v-else-if="!user">Log in to manage collaborators</p>
-    <div v-else class="mt-1 text-sm">No collaborators found</div>
-  </div>
 </template>
