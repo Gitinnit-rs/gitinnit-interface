63a30441562dbecdda4b4ea3fbf812e9a1306249
Vedant Mahadik <mahadikvedant32@gmail.com>
2023-05-12 12:29:19 +0530

    fix: list of collaborators not working fix

--- src/utils/collaborators.ts
+++ src/utils/collaborators.ts
@@ -7,99 +7,101 @@ import { useUserStore } from "../store/user";
  * Get Collaborators
  */
 export async function getCollaborators() {
-  const store = useStore();
-  const { project } = store;
-  const { user } = useUserStore();
+    const store = useStore();
+    const { project } = store;
+    const { user } = useUserStore();
 
-  store.$patch({
-    collaborators: [],
-  });
+    store.$patch({
+        collaborators: [],
+    });
 
-  if (!project || !project.remoteURL) return [];
+    if (!project || !project.remoteURL) return [];
 
-  const url =
-    BASE_URL + `/repos/${user.login}/${project.repo.name}/collaborators`;
+    const url =
+        BASE_URL + `/repos/${user.login}/${project.repo.name}/collaborators`;
 
-  const { data, status } = await axios.get(url, {
-    headers: {
-      Accept: "application/vnd.github+json",
-      Authorization: "Bearer " + user.access_token,
-      "Cache-Control": "no-cache",
-    },
-  });
+    const { data, status } = await axios.get(url, {
+        headers: {
+            Accept: "application/vnd.github+json",
+            Authorization: "Bearer " + user.access_token,
+            // "Cache-Control": "no-cache",
+        },
+    });
 
-  if (status !== 200) throw new Error("Got invalid status code " + status);
+    if (status !== 200) throw new Error("Got invalid status code " + status);
 
-  store.$patch({
-    collaborators: data.slice(1),
-  });
+    store.$patch({
+        collaborators: data,
+    });
 
-  return data.slice(1);
+    return data.slice(1);
 }
 
 /**
  * Add Collaborators
  */
 export async function addCollaborator(username: string) {
-  const {
-    user: { access_token, login },
-  } = useUserStore(); // No need to storeToRefs because one-time use
+    const {
+        user: { access_token, login },
+    } = useUserStore(); // No need to storeToRefs because one-time use
 
-  const store = useStore();
-  const { project } = store;
+    const store = useStore();
+    const { project } = store;
 
-  if (!project || !project.remoteURL) return [];
+    if (!project || !project.remoteURL) return [];
 
-  const url =
-    BASE_URL + `/repos/${login}/${project.repo.name}/collaborators/${username}`;
+    const url =
+        BASE_URL +
+        `/repos/${login}/${project.repo.name}/collaborators/${username}`;
 
-  const { status } = await axios.put(
-    url,
-    {},
-    {
-      headers: {
-        Accept: "application/vnd.github+json",
-        Authorization: "Bearer " + access_token,
-      },
-    }
-  );
+    const { status } = await axios.put(
+        url,
+        {},
+        {
+            headers: {
+                Accept: "application/vnd.github+json",
+                Authorization: "Bearer " + access_token,
+            },
+        }
+    );
 
-  if (status === 204) return false; // Already a collaborator
+    if (status === 204) return false; // Already a collaborator
 
-  if (status !== 201) throw new Error("Got invalid status code " + status);
+    if (status !== 201) throw new Error("Got invalid status code " + status);
 
-  return true; // 201, Invitation created
+    return true; // 201, Invitation created
 }
 
 /**
  * Remove Collaborators
  */
 export async function removeCollaborator(username: string) {
-  const {
-    user: { access_token, login },
-  } = useUserStore(); // No need to storeToRefs because one-time use
+    const {
+        user: { access_token, login },
+    } = useUserStore(); // No need to storeToRefs because one-time use
 
-  const store = useStore();
-  const { project } = store;
+    const store = useStore();
+    const { project } = store;
 
-  if (!project || !project.remoteURL) return [];
+    if (!project || !project.remoteURL) return [];
 
-  const url =
-    BASE_URL + `/repos/${login}/${project.repo.name}/collaborators/${username}`;
+    const url =
+        BASE_URL +
+        `/repos/${login}/${project.repo.name}/collaborators/${username}`;
 
-  const { status } = await axios.delete(url, {
-    headers: {
-      Accept: "application/vnd.github+json",
-      Authorization: "Bearer " + access_token,
-    },
-    data: {
-      owner: login,
-      repo: project.repo.name,
-      username,
-    },
-  });
+    const { status } = await axios.delete(url, {
+        headers: {
+            Accept: "application/vnd.github+json",
+            Authorization: "Bearer " + access_token,
+        },
+        data: {
+            owner: login,
+            repo: project.repo.name,
+            username,
+        },
+    });
 
-  if (status !== 204) throw new Error("Got invalid status code " + status);
+    if (status !== 204) throw new Error("Got invalid status code " + status);
 
-  return true;
+    return true;
 }
