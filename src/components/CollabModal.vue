65c617e1865ee896ff4061796d6ab8600b123c0a
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2023-05-12 22:24:23 +0530

    fix: collab modal duplicate + hide delete for self and owner

--- src/components/CollabModal.vue
+++ src/components/CollabModal.vue
@@ -7,9 +7,9 @@ import { useUserStore } from "../store/user";
 import DeleteIcon from "vue-material-design-icons/Delete.vue";
 import CloseIcon from "vue-material-design-icons/Close.vue";
 import {
-  getCollaborators,
-  addCollaborator,
-  removeCollaborator,
+    getCollaborators,
+    addCollaborator,
+    removeCollaborator,
 } from "../utils/collaborators";
 import { ref } from "vue";
 import { useToast } from "vue-toastification";
@@ -24,101 +24,97 @@ const toast = useToast();
 const username = ref("");
 
 async function invite() {
-  try {
-    const result = await addCollaborator(username.value);
-    if (!result) toast.warning("Already a collaborator");
-    else {
-      toast.success("Invitation sent!");
-      username.value = "";
+    try {
+        const result = await addCollaborator(username.value);
+        if (!result) toast.warning("Already a collaborator");
+        else {
+            toast.success("Invitation sent!");
+            username.value = "";
+        }
+    } catch (e) {
+        console.error("Error while adding collaborator", e);
+        toast.error("Error while adding collaborator");
     }
-  } catch (e) {
-    console.error("Error while adding collaborator", e);
-    toast.error("Error while adding collaborator");
-  }
 }
 
 async function removeCollab(usernameToRemove: any) {
-  console.log("Remove collaborators called");
-  try {
-    await removeCollaborator(usernameToRemove);
+    console.log("Remove collaborators called");
+    try {
+        await removeCollaborator(usernameToRemove);
 
-    setTimeout(() => getCollaborators(), 1000);
+        setTimeout(() => getCollaborators(), 1000);
 
-    toast("Removed as collaborator");
-  } catch (e) {
-    console.error("Error while removing collaborator", e);
-    toast.error("Error while removing collaborator");
-  }
+        toast("Removed as collaborator");
+    } catch (e) {
+        console.error("Error while removing collaborator", e);
+        toast.error("Error while removing collaborator");
+    }
 }
 </script>
 
 <template>
-  <Modal v-model="collabModalOpen">
-    <section>
-      <div class="flex items-center justify-between">
-        <h1 class="font-deca">
-          Collaborators ({{ collaborators.length + 1 }})
-        </h1>
-        <span
-          @click="() => store.$patch({ collabModalOpen: false })"
-          class="mb-2 cursor-pointer text-gray-800"
-        >
-          <CloseIcon />
-        </span>
-      </div>
-
-      <form @submit.prevent="invite" class="flex items-center space-x-2">
-        <input
-          type="text"
-          placeholder="Username"
-          class="mt-2"
-          v-model="username"
-          required
-        />
-
-        <FilledButton class="mt-2">Invite</FilledButton>
-      </form>
-
-      <div>
-        <div
-          class="mt-3 p-3 flex flex-col space-y-3 image-line-list overflow-y-scroll"
-          style="max-height: 250px"
-        >
-          <div class="flex items-center">
-            <img :src="user.avatar_url" v-tooltip="user.login" />
-            <div class="ml-2 transform scale-90">
-              <h3>{{ user.login }}</h3>
-              <p class="text-sm text-gray-500">You</p>
-            </div>
-          </div>
-          <div
-            v-for="(collaborator, i) in collaborators"
-            :key="collaborator.id"
-            class="flex justify-between"
-          >
-            <div class="flex items-center">
-              <img
-                :src="collaborator.avatar_url"
-                v-tooltip="collaborator.login"
-              />
-              <div class="ml-2 transform scale-90">
-                <h3>{{ collaborator.login }}</h3>
-                <p class="text-sm text-gray-500">{{ collaborator.type }}</p>
-              </div>
+    <Modal v-model="collabModalOpen">
+        <section>
+            <div class="flex items-center justify-between">
+                <h1 class="font-deca">
+                    Collaborators ({{ collaborators.length + 1 }})
+                </h1>
+                <span
+                    @click="() => store.$patch({ collabModalOpen: false })"
+                    class="mb-2 cursor-pointer text-gray-800"
+                >
+                    <CloseIcon />
+                </span>
             </div>
 
-            <div
-              class="grid place-items-center"
-              @click="removeCollab(collaborator.login)"
-            >
-              <component
-                :is="DeleteIcon"
-                class="text-red-500 text-2xl mb-1.5 cursor-pointer"
-              />
+            <form @submit.prevent="invite" class="flex items-center space-x-2">
+                <input
+                    type="text"
+                    placeholder="Username"
+                    class="mt-2"
+                    v-model="username"
+                    required
+                />
+
+                <FilledButton class="mt-2">Invite</FilledButton>
+            </form>
+
+            <div>
+                <div
+                    class="mt-3 p-3 flex flex-col space-y-3 image-line-list overflow-y-scroll"
+                    style="max-height: 250px"
+                >
+                    <div
+                        v-for="(collaborator, i) in collaborators"
+                        :key="collaborator.id"
+                        class="flex justify-between"
+                    >
+                        <div class="flex items-center">
+                            <img
+                                :src="collaborator.avatar_url"
+                                v-tooltip="collaborator.login"
+                            />
+                            <div class="ml-2 transform scale-90">
+                                <h3>{{ collaborator.login }}</h3>
+                                <p class="text-sm text-gray-500">
+                                    {{ collaborator.type }}
+                                </p>
+                            </div>
+                        </div>
+
+                        <div
+                            v-if="collaborator.login !== user.login"
+                            class="grid place-items-center"
+                            @click="removeCollab(collaborator.login)"
+                        >
+                            <component
+                                :is="DeleteIcon"
+                                class="text-red-500 text-2xl mb-1.5 cursor-pointer"
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  </Modal>
+        </section>
+    </Modal>
 </template>
